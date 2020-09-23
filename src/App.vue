e042b650ed6a2645ae57de31df90843d9e62345a
fx67ll <2717745014@qq.com>
2020-09-23 17:09:22 +0800

    delete consolelog

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
 	<div id="app">
-		<router-view v-show="isDomOK"></router-view>
+		<router-view></router-view>
 		<vueCanvasNest :config="nestConfig" :el="'#app'"></vueCanvasNest>
-		<!-- <div class="loader" v-show="!isDomOK">
+		<!-- <div class="loader">
 			<div class="loader-inner pacman">
 				<div></div>
 				<div></div>
@@ -24,7 +24,6 @@ export default {
 	},
 	data() {
 		return {
-			isDomOK: false,
 			nestConfig: {
 				color: 'rgb(186, 186, 186)', // the canvas line color, default: '255,0,0'; the color is (R,G,B)
 				opacity: 0.7, // the opacity of line (0~1), default: 0.7
@@ -34,23 +33,7 @@ export default {
 			time: 0
 		};
 	},
-	created() {
-		this.checkDomInit();
-	},
 	methods: {
-		checkDomInit() {
-			var self = this;
-			this.isDomOK = false;
-			this.time = new Date().getTime();
-			document.onreadystatechange = completeLoading;
-			function completeLoading() {
-				if (document.readyState == 'complete') {
-					self.time = new Date().getTime() - self.time;
-					console.log('网页加载共耗时：' + self.time);
-					self.isDomOK = true;
-				}
-			}
-		}
 	}
 };
 </script>
@@ -74,10 +57,5 @@ body {
 	padding: 10px;
 	background-color: #42b983;
 	border-radius: 12px;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	margin-top: -20px;
-	margin-left: -50px;
 }
 </style>
