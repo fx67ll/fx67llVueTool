139303093028f510487f326f85a3d0b872d607b7
fx67ll <2717745014@qq.com>
2021-01-06 18:06:56 +0800

    添加elementui依赖

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
 import Vue from 'vue'
 import App from '@/App.vue'
+import router from '@/router/index.js'
+import store from '@/store/index.js'
 
 Vue.config.productionTip = false
 
 import less from 'less'
 Vue.use(less)
 
-import router from '@/router/index.js'
-
 import VueCodeMirror from 'vue-codemirror'
 import 'codemirror/lib/codemirror.css'
 Vue.use(VueCodeMirror)
 
-import store from '@/store/index.js'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 new Vue({
 	router,
